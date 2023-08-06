9f747ab3fe5aee9d4450cc64f6a0d83c560e3db9
FlowerCoder <hosibito@naver.com>
2023-08-06 13:05:29 +0900

    02강.  HTML에 데이터 꽂아넣는 Vue 데이터바인딩 문법

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h4 :style="styleH4">{{ products[0] }}</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>XX 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  data(){
+    return{
+      price1 :60,
+      styleH4 : 'color: red',
+      products : ['역삼동 원룸', '천호동 원룸', '마포구 원룸']
+    }
   }
 }
 </script>
