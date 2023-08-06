35d5ae3e40d3cf9603875a10552af34f83de17f5
FlowerCoder <hosibito@naver.com>
2023-08-06 13:53:00 +0900

    04강. Vue 이벤트 핸들러로 click 감지하기

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,34 @@
     <a v-for="menu in menuList" :key="menu">{{ menu }}</a>    
   </div>
 
-  <div class="menu">
-    <a v-for="(menu, index) in menuList" :key="index">{{ menu }} / {{ index }}</a>    
-  </div>
-
-  <div v-for="product in products" :key="product">
-    <h4>{{ product }}</h4>
-    <p>XX 만원</p>
-  </div>
-
+  <div v-for="(product,index) in products" :key="product.name">
+    <h4>{{product.name}}</h4>
+    <p>{{product.price}}만원</p>
+    <button @click="addDeclaration(index)">허위매물신고</button>
+    <span>신고수 : {{product.declaration}}</span>
+  </div>  
 </template>
 
 <script>
-
-
 export default {
   name: 'App',
   components: {
-
   },
   data(){
     return{
       menuList : ['Home', 'Products', 'About'],
       price1 :60,
       styleH4 : 'color: red',
-      products : ['역삼동 원룸', '천호동 원룸', '마포구 원룸']
+      products : [
+        { name : '역삼동 원룸', price : 80, declaration: 0},
+        { name : '천호동 원룸', price : 50, declaration: 0},
+        { name : '마포구 원룸', price : 70, declaration: 0},
+      ]    
+    }
+  },
+  methods : {
+    addDeclaration(index){
+      this.products[index].declaration += 1;
     }
   }
 }
@@ -53,3 +56,4 @@ export default {
   padding : 10px;
 }
 </style>
+
